92f0d01475ed82563bd2cff5d3320dad6091fe8d
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-07-23 16:36:01 +0200

    added build script with uf2 conversion

---
+++ app/build-hs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# To build hs22k for nice!nano
+west build -p --board hs22k
+python3 ../../../uf2conv/uf2conv.py build/zephyr/zmk.hex --family 0xADA52840 --convert --output ../../../Desktop/hs22k.uf2
