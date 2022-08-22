33770a2ed83a027effe5f2252affc3e9907ac972
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-08-22 15:28:48 +0200

    moved speial symbols and added more tapdances

--- app/build-hs.sh
+++ app/build-hs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # To build hs22k for nice!nano
+export ZEPHYR_TOOLCHAIN_VARIEANT=zephyr
 west build -p --board hs22k
 python3 ../../../uf2conv/uf2conv.py build/zephyr/zmk.hex --family 0xADA52840 --convert --output ../../../Desktop/hs22k.uf2
