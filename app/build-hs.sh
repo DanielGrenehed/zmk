1c6824f9f1251e234aec5adfefe0e97c6443f01d
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2023-06-30 23:32:14 +0200

    hs22k out_ble on profile-select

--- app/build-hs.sh
+++ app/build-hs.sh
@@ -2,4 +2,4 @@
 # To build hs22k for nice!nano
 export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
 west build -p --board hs22k
-python3 ../../../uf2conv/uf2conv.py build/zephyr/zmk.hex --family 0xADA52840 --convert --output ../../../Desktop/hs22k.uf2
+#python3 ../../../uf2conv/uf2conv.py build/zephyr/zmk.hex --family 0xADA52840 --convert --output ../../../Desktop/hs22k.uf2
